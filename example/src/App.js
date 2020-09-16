fb8875c1fec12685ec1c152ec5e261a7045f3f4a
Volodymyr Krasnoshapka <vkr@jysk.com>
2020-09-16 16:53:29 +0300

    Initiated build via rollup and Examples project via create-react-app.

---
+++ example/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Package from "react-image-tooltips";
+
+function App() {
+  return (
+    <div className="App">
+        <Package />
+    </div>
+  );
+}
+
+export default App;
