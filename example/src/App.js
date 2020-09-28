515453ec32c0c2545fb76dfc913346ee278524dc
Volodymyr Krasnoshapka <vkr@jysk.com>
2020-09-28 17:25:54 +0300

    Changed text.

--- example/src/App.js
+++ example/src/App.js
@@ -9,12 +9,12 @@ function App() {
 
         <ImageTooltips src="example.jpg" width={816} height={544}>
             <ImageTooltipsItem top={226} left={301}>
-                <p>Here's some content that lies inside an accordion.</p>
-                <p>This content can only be viewed, once the Accordion is toggled.</p>
+                <p>Here's some content that lies inside an tooltip.</p>
+                <p>This content can only be viewed, once the tooltip is toggled.</p>
             </ImageTooltipsItem>
             <ImageTooltipsItem top={300} left={504}>
-                <p>Here's some content that lies inside an accordion.</p>
-                <p>This content can only be viewed, once the Accordion is toggled.</p>
+                <p>Here's some content that lies inside an tooltip.</p>
+                <p>This content can only be viewed, once the tooltip is toggled.</p>
             </ImageTooltipsItem>
         </ImageTooltips>
 
