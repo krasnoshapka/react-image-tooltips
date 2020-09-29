9c677d6d1b2b830dceb65d1dc5ba1a396d675ca5
Volodymyr Krasnoshapka <shlapa@gmail.com>
2020-09-29 16:48:29 +0300

    Updates

---
+++ precache-manifest.e159616269348b5857e8e982fd69ca02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "094cb84709934e300f412dfb8e914ef5",
+    "url": "/react-image-tooltips/index.html"
+  },
+  {
+    "revision": "b6be504653bd3e859a20",
+    "url": "/react-image-tooltips/static/css/main.a36f7f40.chunk.css"
+  },
+  {
+    "revision": "3d9b2a571d00f472e026",
+    "url": "/react-image-tooltips/static/js/2.016c2508.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-image-tooltips/static/js/2.016c2508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6be504653bd3e859a20",
+    "url": "/react-image-tooltips/static/js/main.5b319020.chunk.js"
+  },
+  {
+    "revision": "458ea099d61fe61b24da",
+    "url": "/react-image-tooltips/static/js/runtime-main.f9b4fc16.js"
+  }
+]);
