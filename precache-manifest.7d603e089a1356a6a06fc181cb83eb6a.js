fa8f077c9369a427a6fe8b0e13f77a32d1109acb
Volodymyr Krasnoshapka <shlapa@gmail.com>
2020-10-01 16:34:04 +0300

    Updates

---
+++ precache-manifest.7d603e089a1356a6a06fc181cb83eb6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a085de6af3dea13170a88d6f6901353d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea593093e21733533965",
+    "url": "/static/css/2.a36f7f40.chunk.css"
+  },
+  {
+    "revision": "ea593093e21733533965",
+    "url": "/static/js/2.6dd17ef9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6dd17ef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "108d7dde8f9c199bbacc",
+    "url": "/static/js/main.04508da3.chunk.js"
+  },
+  {
+    "revision": "478cfcc8890942f20bcc",
+    "url": "/static/js/runtime-main.faa57237.js"
+  }
+]);
